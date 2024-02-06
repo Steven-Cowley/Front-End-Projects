815291578b9d282ebfb762ae0343dfacf97d783c
Steven-Cowley <144847999+Steven-Cowley@users.noreply.github.com>
2024-02-05 23:09:18 -0500

    index.js
    added comments

--- Age_Calculator/index.js
+++ Age_Calculator/index.js
@@ -1,10 +1,13 @@
 function handleInput() {
+    // stores the value of the year given by the user
     let userIn = document.getElementById("yearIn").value;
+    // check if user has enterd a number
     if (isNaN(userIn)) {
         alert("Invalid entry please enter a year");
         return;
     }
     let year = 2024;
+    // calculate age
     let userAge = year - userIn;
     handleOutput(userAge);
 }
